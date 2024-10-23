437dec53f4f0b666de66bcafb3419362e36a0dbb
garthdp <garthdp4@gmail.com>
2024-10-23 13:39:33 +0200

    commit

--- SPCAAPI/obj/Debug/net8.0/SPCAAPI.AssemblyInfo.cs
+++ SPCAAPI/obj/Debug/net8.0/SPCAAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPCAAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52443b18a94e547392cdc0b3cb03e49cd5e4ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8476457a0ec422b6219caa06e253a53dd318673")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPCAAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPCAAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
